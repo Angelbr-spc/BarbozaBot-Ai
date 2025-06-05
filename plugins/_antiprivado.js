189d01b678f77ddb1b78a33f214f8ef7f1990827
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-05 16:16:01 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,16 +1,14 @@
-
-export async function before(m, { conn, isOwner, isROwner}) {
-    if (m.isBaileys && m.fromMe) return true;
-    if (m.isGroup) return false;
-    if (!m.message) return true;
-
-    const botSettings = global.db.data.settings[this.user.jid] || {};
-
-    if (botSettings.antiPrivate &&!isOwner &&!isROwner) {
-        await conn.updateBlockStatus(m.chat, 'block'); // Bloquea al usuario sin enviar mensaje
-        console.log(`Usuario ${m.sender} bloqueado por contacto privado.`);
-}
-
-    return false;
-}
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isRowner, isMods}) {
+    if (m.isBaileys && m.fromMe) return !0;
+    if (m.isGroup) return !1;
+    if (!m.message) return !0;
+    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('code') || m.text.includes('qr')) return !0;
+    const chat = global.db.data.chats[m.chat];
+    const bot = global.db.data.settings[this.user.jid] || {};
+    if (bot.antiPrivate && !isOwner && !isROwner && !isMods) {
+      await m.reply(`> 《★》@${m.sender.split`@`[0]} Está Prohibido Escribir Al Privado Del Bot.\n> Si Deseas Comprar nuestro bot Escribe al: 7227584934`, false, {mentions: [m.sender]});
+      await this.updateBlockStatus(m.chat, 'block');
+    }
+    return !1;
+  }
  
